46218209eafff89ebe91e2d44d4caaa11810262b
Kranti18kiran <nanikiri@gmail.com>
2024-07-14 21:05:13 +1000

    loop adding

--- tests/BasicE2Etests.spec.js
+++ tests/BasicE2Etests.spec.js
@@ -28,9 +28,25 @@ test.only("Basic Test 1 page fixture", async ({page})=>  {
         console.log(trimspaces_content);
         const textconsltlistitem = await consultlistitems.count();
         console.log(textconsltlistitem);
-        const closesconsult = await page.locator('[class="field__label"]').getByText('Closes').count();
-        const closedconsult = await page.locator('[class*="field__label"]').getByText('Closed').count();
-        console.log(closesconsult, closedconsult);
+        const closesconsult = await page.locator('[class="field__label"]').getByText('Closes');
+        const closescount = await closesconsult.count();
+        const closedconsult = await page.locator('[class*="field__label"]').getByText('Closed');
+        const closedcount = await closedconsult.count();
+
+
+        console.log(closescount, closedcount);
+
+        for(let i=0; i < closescount; i++){
+
+             const nodeTitle = await page.locator(".node__title").nth(i);
+             const nodeTitlecontent = await nodeTitle.textContent();
+            console.log(nodeTitlecontent);
+             await nodeTitle.click();
+             await expect(page.locator(".consultation-submission")).toBeVisible();
+             await page.goBack();
+
+        }
+
 
     
     });
